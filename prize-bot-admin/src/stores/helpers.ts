8bffe6a4d63d5533f5c2785c85103790fe40269f
Z0maxx <nyari.zoltan500@gmail.com>
2026-07-11 16:16:21 +0200

    added snackbar messages

--- prize-bot-admin/src/stores/helpers.ts
+++ prize-bot-admin/src/stores/helpers.ts
@@ -1,16 +1,18 @@
 import { useAppStore } from './app'
+import { useSnackbarStore } from './snackbar'
 
 import type { DataResult, Result } from '@jf-prize-bot/schema'
 import type { Ref } from 'vue'
 
 export async function load<T>(at: string, ref: Ref<T>, callback: () => Promise<DataResult<T>>) {
   const { addIsLoading, removeIsLoading } = useAppStore()
+  const { error } = useSnackbarStore()
   addIsLoading(at)
   const result = await callback()
   if (result.success) {
     ref.value = result.data!
   } else {
-    alert(`Failed to load ${at}: ${result.error}`)
+    error(`Failed to load ${at}: ${result.error}`)
   }
 
   removeIsLoading(at)
@@ -18,12 +20,13 @@ export async function load<T>(at: string, ref: Ref<T>, callback: () => Promise<D
 
 export async function save(at: string, callback: () => Promise<Result>) {
   const { addIsSaving, removeIsSaving, removeHasChanges } = useAppStore()
+  const { error } = useSnackbarStore()
   addIsSaving(at)
   const result = await callback()
   if (result.success) {
     removeHasChanges(at)
   } else {
-    alert(`Failed to save ${at}: ${result.error}`)
+    error(`Failed to save ${at}: ${result.error}`)
   }
 
   removeIsSaving(at)
